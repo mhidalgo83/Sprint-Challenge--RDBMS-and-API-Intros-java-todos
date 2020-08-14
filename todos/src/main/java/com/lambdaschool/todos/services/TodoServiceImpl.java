60fe849bcf4728941f6f4a9bb8b1df2ebf9e686d
Matthew Hidalgo <makeshifthidalgo@hotmail.com>
2020-08-14 08:47:50 -0700

    mvp complete

--- todos/src/main/java/com/lambdaschool/todos/services/TodoServiceImpl.java
+++ todos/src/main/java/com/lambdaschool/todos/services/TodoServiceImpl.java
@@ -1,9 +1,12 @@
 package com.lambdaschool.todos.services;
 
+import com.lambdaschool.todos.models.Todo;
 import com.lambdaschool.todos.repository.TodoRespository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityNotFoundException;
 
 @Transactional
 @Service(value = "todoService")
@@ -15,6 +18,7 @@ public class TodoServiceImpl implements TodosService {
 
     @Override
     public void markComplete(long todoid) {
-
+        Todo todo = todorepos.findById(todoid).orElseThrow(() -> new EntityNotFoundException("Todo " + " Not Found"));
+        todo.setCompleted(true);
     }
 }
